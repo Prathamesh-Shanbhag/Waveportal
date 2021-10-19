2c6d857aa812c58e03b1c3b4d56d170e2c9f0557
Prathamesh-Shanbhag <prathameshshanbhag074@gmail.com>
2021-10-19 11:47:45 +0530

    refactor:Sentence Updated :pencil2:

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Prathamesh and I am currently working on this App so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
